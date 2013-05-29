a6741bdca1b0dbbe8943c2477aa1c98191f4e87f
Benny Tsai <benny.tsai@gmail.com>
2013-05-29 00:59:17 -0700

    tweak weather unit conversions, clean-up

--- src/jarvis/commands/weather.clj
+++ src/jarvis/commands/weather.clj
@@ -19,10 +19,10 @@
      (speech/say! (format "good evening. it's %s." time-str)))))
 
 (defn K->F [x]
-  (-> x (- 273.15) (* 1.8) (+ 32) int (str " degrees")))
+  (-> x (- 273.15) (* (/ 9 5)) (+ 32) int (str " degrees")))
 
 (defn mps->MPH [x]
-  (-> x (* 2.2369362920544) int (str " mph")))
+  (-> x (* 2.23694) int (str " mph")))
 
 (defn forecast [id]
   (let [fs (-> "http://api.openweathermap.org/data/2.5/forecast"
@@ -30,32 +30,34 @@
                (:body)
                (:list))
         temps (->> fs
-                   (take 8)  ;; take next 24 hours
+                   (take 8) ;; take next 24 hours
                    (map #(-> % :main :temp)))]
     {:high (->> temps (apply max) K->F)
      :low (->> temps (apply min) K->F)}))
 
-(defn weather [location]
+(defn weather [city]
   (let [w (-> "http://api.openweathermap.org/data/2.5/weather"
-              (http/get {:query-params {:q location} :as :json})
+              (http/get {:query-params {:q city} :as :json})
               (:body))]
-    {:condition (-> w :weather first :description)
+    {:desc (-> w :weather first :description)
      :humidity (-> w :main :humidity (str "%"))
      :id (-> w :id)
      :name (-> w :name)
      :temp (-> w :main :temp K->F)
      :wind (-> w :wind :speed mps->MPH)}))
 
-(defn announce! [location]
-  (let [{:keys [condition humidity id name temp wind]} (weather location)
+(defn announce! [city]
+  (let [{:keys [desc humidity id name temp wind]} (weather city)
         {:keys [high low]} (forecast id)
         line1 "the weather in %s is %s with %s."
-        line2 "wind: %s. humidity: %s."
-        line3 "24-hour forecast: high of %s and low of %s."]
+        line2 "the humidity is %s with %s winds."
+        line3 "sunrise will be %s and sunset will be %s."
+        line4 "the 24-hour forecast is high of %s and low of %s."]
     (announce-time!)
-    (speech/say! (format line1 name temp condition))
-    (speech/say! (format line2 wind humidity))
-    (speech/say! (format line3 high low))))
+    (speech/say! (format line1 name temp desc))
+    (speech/say! (format line2 humidity wind))
+    ;; (speech/say! (format line3 sunrise sunset))
+    (speech/say! (format line4 high low))))
 
 (def commands
   [{:cmd ["weather"] :fn (fn [ws] (announce! (str/join " " ws)))}])

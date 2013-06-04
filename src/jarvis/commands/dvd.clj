c6e22aa0e3db2bd69a3fa6a0f0cbdfcbe0db4786
Benny Tsai <benny.tsai@gmail.com>
2013-06-04 01:27:43 -0700

    add DVD audio and chapter selection

--- src/jarvis/commands/dvd.clj
+++ src/jarvis/commands/dvd.clj
@@ -5,6 +5,18 @@
 (defn do! [cmd]
   (osa/do! "DVD player" cmd))
 
+(defn set-audio! [word]
+  (try
+    (do! (str "set audio track to " (Integer. word)))
+    (catch Exception e
+      (speech/say! "audio track should be an integer."))))
+
+(defn set-chapter! [word]
+  (try
+    (do! (str "set chapter to " (Integer. word)))
+    (catch Exception e
+      (speech/say! "chapter should be an integer."))))
+
 (defn set-fullscreen! [word]
   (case word
     "on"  (do! "set viewer full screen to true")
@@ -12,17 +24,17 @@
     (speech/say! "please say on or off.")))
 
 (defn set-subtitle! [word]
-  (case word
-    "on"  (do! "set subtitle to 1")
-    "off" (do! "set subtitle to 0")
-    (speech/say! "please say on or off.")))
+  (try
+    (do! (str "set subtitle to " (Integer. word)))
+    (catch Exception e
+      (speech/say! "subtitle should be an integer."))))
 
 (def commands
-  [{:cmd ["dvd" "play"]       :fn (fn [_] (do! "play dvd"))}
-   {:cmd ["dvd" "stop"]       :fn (fn [_] (do! "pause dvd"))}
-   {:cmd ["dvd" "next"]       :fn (fn [_] (do! "play next chapter"))}
-   {:cmd ["dvd" "previous"]   :fn (fn [_] (do! "play previous chapter"))}
-   {:cmd ["dvd" "quit"]       :fn (fn [_] (do! "quit"))}
+  [{:cmd ["dvd" "audio"]      :fn (fn [[w]] (set-audio! w))}
+   {:cmd ["dvd" "chapter"]    :fn (fn [[w]] (set-chapter! w))}
    {:cmd ["dvd" "eject"]      :fn (fn [_] (do! "eject dvd"))}
    {:cmd ["dvd" "fullscreen"] :fn (fn [[w]] (set-fullscreen! w))}
+   {:cmd ["dvd" "play"]       :fn (fn [_] (do! "play dvd"))}
+   {:cmd ["dvd" "quit"]       :fn (fn [_] (do! "quit"))}
+   {:cmd ["dvd" "stop"]       :fn (fn [_] (do! "pause dvd"))}
    {:cmd ["dvd" "subtitle"]   :fn (fn [[w]] (set-subtitle! w))}])

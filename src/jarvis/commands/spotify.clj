bafeca1b6e23553a61d20f3247e9cbc7151942e2
Benny Tsai <benny.tsai@gmail.com>
2013-06-23 16:53:59 -0700

    add spotify play track command

--- src/jarvis/commands/spotify.clj
+++ src/jarvis/commands/spotify.clj
@@ -7,18 +7,29 @@
 (defn do! [cmd]
   (fn [_] (osa/do! "Spotify" cmd)))
 
-(defn get-album [query]
-  (-> "http://ws.spotify.com/search/1/album.json"
+(defn get-item [item query]
+  (-> (str "http://ws.spotify.com/search/1/" item ".json")
       (http/get {:query-params {:q query} :as :json})
       (:body)
-      (:albums)
-      (->> (filter #(->> % :availability :territories (re-find #"US"))))
+      (get (keyword (str item "s")))
       (first)))
 
 (defn play-album! [words]
-  (if-let [{:keys [href]} (get-album (str/join " " words))]
-    (osa/do! "Spotify" (str "play track \"" href "\""))
-    (speech/say! "album not found.")))
+  (if-let [{:keys [href]} (get-item "album" (str/join " " words))]
+    (osa/do! "Spotify" (format "play track \"%s\"" href))
+    (speech/say! (str "album not found."))))
+
+(defn play-track! [words]
+  (if-let [{:keys [album href]} (get-item "track" (str/join " " words))]
+    (do
+      (osa/do! "Spotify" (format "play track \"%s\" in context \"%s\""
+                                 href
+                                 (:href album)))
+      ;; HACK to fix repeat being disabled when playing a track
+      (Thread/sleep 1000)
+      (osa/do! "Spotify" "set repeating to false")
+      (osa/do! "Spotify" "set repeating to true"))
+    (speech/say! (str "track not found."))))
 
 (def commands
   [{:cmd ["spotify" "album"]    :fn play-album!}
@@ -26,4 +37,5 @@
    {:cmd ["spotify" "previous"] :fn (do! "previous track")}
    {:cmd ["spotify" "quit"]     :fn (do! "quit")}
    {:cmd ["spotify" "resume"]   :fn (do! "play")}
-   {:cmd ["spotify" "stop"]     :fn (do! "pause")}])
+   {:cmd ["spotify" "stop"]     :fn (do! "pause")}
+   {:cmd ["spotify" "track"]    :fn play-track!}])

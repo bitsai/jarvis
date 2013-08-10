6143908f653813fe721cb7cd70f4fc15859f2856
Benny Tsai <benny.tsai@gmail.com>
2013-08-09 18:58:19 -0700

    catch up to go-jarvis

--- src/jarvis/commands/wolfram.clj
+++ src/jarvis/commands/wolfram.clj
@@ -1,6 +1,7 @@
 (ns jarvis.commands.wolfram
   (:require [clj-http.client :as http]
-            [clojure.data.zip.xml :as zip-xml]
+            [clojure.data.zip :as zf]
+            [clojure.data.zip.xml :as zf-xml]
             [clojure.string :as str]
             [clojure.xml :as xml]
             [clojure.zip :as zip]))
@@ -16,15 +17,23 @@
         (xml/parse)
         (zip/xml-zip))))
 
+;; use this because zf-xml/text snarfs newline characters
+(defn text [loc]
+  (apply str (zf-xml/xml-> loc zf/descendants zip/node string?)))
+
+(defn clean [s]
+  (-> s
+      (str/replace "|" ",")
+      (str/replace "\n" ",\n")
+      (str/replace "°F" "° Fahrenheit")))
+
 (defn ask [s]
   (let [z (query s)
-        subPods (zip-xml/xml-> z :pod :subpod)]
-    (if (seq subPods)
-      (->> (for [sp subPods
-                 :let [title ((zip-xml/attr :title) sp)
-                       text (zip-xml/text sp)]]
-             (str title ": " text))
-           (str/join ", "))
+        subpods (zf-xml/xml-> z :pod :subpod)]
+    (if (seq subpods)
+      (->> subpods
+           (mapcat (juxt (zf-xml/attr :title) (comp clean text)))
+           (str/join ",\n"))
       (throw (Exception. "No answers found.")))))
 
 (def commands

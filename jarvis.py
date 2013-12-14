32f7166ebfd920bbf327f7a8bc42373c373ab235
Benny Tsai <benny.tsai@gmail.com>
2013-12-14 02:07:36 -0800

    add frontend SL4A files

---
+++ jarvis.py
@@ -0,0 +1,19 @@
+import android
+import sys
+import urllib
+
+droid = android.Android()
+html = "file:///sdcard/sl4a/scripts/jarvis.html"
+url = "http://192.168.1.2:8080"
+
+droid.webViewShow(html)
+
+while True:
+  input = droid.eventWait().result["data"]
+  if input == "exit":
+    sys.exit()
+  try:
+    output = urllib.urlopen(url, input).read()
+  except Exception as e:
+    output = str(e)
+    droid.eventPost("output", output)

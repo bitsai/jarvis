4c63a7fd711d26006c14e4145085c0bd2e1b2773
Benny Tsai <benny.tsai@gmail.com>
2014-08-24 13:43:39 -0700

    switch to Wolfram Alpha image results, for #5

--- src/jarvis/commands/wolfram.clj
+++ src/jarvis/commands/wolfram.clj
@@ -1,6 +1,5 @@
 (ns jarvis.commands.wolfram
-  (:require [clojure.data.zip :as zf]
-            [clojure.data.zip.xml :as zf-xml]
+  (:require [clojure.data.zip.xml :as zf-xml]
             [clojure.string :as str]
             [clojure.xml :as xml]
             [clojure.zip :as zip]
@@ -8,46 +7,34 @@
             [org.httpkit.client :as http]))
 
 (defn query [app-id s]
-  (let [params {:appid app-id :format "plaintext" :input s}]
+  (let [params {:appid app-id :format "image" :input s}]
     (-> "http://api.wolframalpha.com/v2/query"
         (http/get {:query-params params :as :stream})
         (deref)
         (:body))))
 
-;; use this because zf-xml/text snarfs newline characters
-(defn ->text [z]
-  (apply str (zf-xml/xml-> z zf/descendants zip/node string?)))
+(defn parse-errors [zipper]
+  (->> (zf-xml/xml-> zipper :error :msg)
+       (map zf-xml/text)))
 
-(defn no-leading-newline [s]
-  (str/replace s #"^\n" ""))
-
-(defn parse-subpod [subpod]
-  (let [title (zf-xml/attr subpod :title)
-        text (-> subpod ->text no-leading-newline)]
-    (when (seq text)
-      (if (seq title)
-        (str/join "\n" [title text])
-        text))))
-
-(defn parse-pod [pod]
-  (let [title (zf-xml/attr pod :title)
-        texts (->> (zf-xml/xml-> pod :subpod)
-                   (keep parse-subpod)
-                   (str/join "\n"))]
-    (when (seq texts)
-      (str/join "\n" [title texts]))))
+(defn parse-results [zipper]
+  (->> (zf-xml/xml-> zipper :pod)
+       (map (fn [pod]
+              (let [title (zf-xml/attr pod :title)
+                    img-src (-> pod
+                                (zf-xml/xml-> :subpod :img)
+                                (first)
+                                (zf-xml/attr :src))]
+                (format "%s<br><img src=\"%s\">" title img-src))))))
 
 (defn parse-xml [xml]
   (let [z (-> xml xml/parse zip/xml-zip)
-        pods (zf-xml/xml-> z :pod)]
-    (if (seq pods)
-      (->> pods
-           (keep parse-pod)
-           (str/join "\n\n"))
-      "no results found")))
+        errors (parse-errors z)
+        results (parse-results z)]
+    (cond
+     (seq errors)  errors
+     (seq results) results
+     :else         ["no results found"])))
 
 (defn ask [s]
-  (let [app-id (e/env :app-id)]
-    (if (seq app-id)
-      (-> app-id (query s) parse-xml)
-      "Wolfram Alpha app id not found!")))
+  (->> s (query (e/env :app-id)) parse-xml (str/join "<br><br>")))

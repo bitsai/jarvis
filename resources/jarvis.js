5785e8344c8ac0b52df7ad3c150be9ba8454a515
Benny Tsai <benny.tsai@gmail.com>
2014-08-28 01:43:33 -0700

    add HTML5 speech recognition, closes #5

---
+++ resources/jarvis.js
@@ -0,0 +1,32 @@
+var listenButton = document.getElementById("listen");
+
+if (!('webkitSpeechRecognition' in window)) {
+    listenButton.disabled = true;
+} else {
+    var recognizer = new webkitSpeechRecognition();
+    var recognizing = false;
+
+    recognizer.onend = function(event) {
+        listenButton.innerHTML = "Listen";
+        recognizing = false;
+    }
+
+    recognizer.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        document.getElementById("input").value = transcript;
+    }
+
+    recognizer.onstart = function(event) {
+        listenButton.innerHTML = "Listening";
+    }
+
+    function recognize() {
+        if (recognizing) {
+            recognizer.stop();
+        } else {
+            listenButton.innerHTML = "Initializing";
+            recognizing = true;
+            recognizer.start();
+        }
+    }
+}
--- src/jarvis/html.clj
+++ src/jarvis/html.clj
@@ -1,9 +1,19 @@
 (ns jarvis.html
-  (require [hiccup.core :as h]
+  (require [clojure.java.io :as io]
+           [hiccup.core :as h]
+           [hiccup.element :as he]
            [hiccup.form :as hf]))
 
 (defn render [outputs]
   (h/html (hf/form-to [:post "/"]
                       (hf/text-field "input")
+                      [:button
+                       {:id "listen"
+                        :onclick "recognize()"
+                        :type "button"}
+                       "Listen"]
                       (hf/submit-button "Submit"))
-          (interpose "<br><br>" outputs)))
+          (interpose "<br><br>" outputs)
+          (he/javascript-tag (-> "jarvis.js"
+                                 (io/resource)
+                                 (slurp)))))

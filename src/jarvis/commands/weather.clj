ea6b9b8a8fc881fcd811cec71f63764e3a4b9030
Benny Tsai <benny.tsai@gmail.com>
2016-04-13 17:35:44 -0700

    optimize weather command

--- src/jarvis/commands/weather.clj
+++ src/jarvis/commands/weather.clj
@@ -18,16 +18,22 @@
    :forecast-today (parse-data #"Today\n(.*)\n" weather)})
 
 (defn announce! [location]
-  (basic/say! (format "Hello, it's %s." (->local-time)))
-  (let [weather (wolfram/ask! (format "weather near %s" location))]
-    (if (= weather "no results found")
-      (basic/say! weather)
-      (let [parsed-weather (parse-weather weather)]
-        (basic/say! (format "The weather in %s is %s, %s, with winds at %s."
-                            (:location parsed-weather)
-                            (:temperature parsed-weather)
-                            (:conditions parsed-weather)
-                            (:wind parsed-weather)))
-        (basic/say! (format "Today's temperature will be %s."
-                            (:forecast-today parsed-weather)))))
+  ;; run in future so we load weather while saying the greeting
+  (future (basic/say! (format "Hello, it's %s." (->local-time))))
+  (let [weather (wolfram/ask! (format "weather near %s" location)
+                              ;; only get the pods we need
+                              {:podindex "2,3"})]
+    ;; run in future so we don't block on text-to-speech
+    (future (if (= weather "no results found")
+              (basic/say! weather)
+              (let [parsed-weather (parse-weather weather)]
+                (basic/say! (format "The weather in %s is %s, %s."
+                                    (:location parsed-weather)
+                                    (:temperature parsed-weather)
+                                    (:conditions parsed-weather)))
+                (when-let [wind (:wind parsed-weather)]
+                  (basic/say! (format "With winds at %s."
+                                      wind)))
+                (basic/say! (format "Today's temperature will be %s."
+                                    (:forecast-today parsed-weather))))))
     weather))
